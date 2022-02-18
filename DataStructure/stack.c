25916f716d248b9024d05f6b3d92de9efb069c01
PTFDB <kbs95123@gmail.com>
2022-02-18 21:52:57 +0900

    stack완성

--- DataStructure/stack.c
+++ DataStructure/stack.c
@@ -24,6 +24,19 @@ void Push(Stack** top, int data)
 	(*top)->link = tmp;
 }
 
+void Pop(Stack** top) {
+	if (*top) {
+		Stack* tmp;
+		printf("data: %d\n", (*top)->data);
+		tmp = *top;
+		*top = (*top)->link;
+		free(tmp);
+	}
+	else {
+		printf("Stack is Empty!!");
+	}
+}
+
 int main()
 {
 	Stack* top = EMPTY;
@@ -31,6 +44,11 @@ int main()
 	Push(&top, 10);
 	Push(&top, 20);
 	Push(&top, 30);
+
+	Pop(&top);
+	Pop(&top);
+	Pop(&top);
+	Pop(&top);
 
 	return 0;
 }
